6f48d17d2e179279c737e32853cbcbc27343467f
Michael Durfey <mdurfey@me.com>
2017-09-01 15:45:37 -0700

    basic server

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+let port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs')
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) =>{
+    res.sendFile(__dirname + '/index.html');
+})
+
+
+
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!');
+})
