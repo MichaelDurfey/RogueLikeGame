75047766cb9ff6e03d1f781a0f51029ee31973ac
Michael Durfey <mdurfey@me.com>
2017-09-01 18:45:56 -0700

    environment set

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ let port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs')
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/node_modules'));
+app.use(express.static(__dirname + '/lib'));
 
 app.get('/', (req, res) =>{
     res.sendFile(__dirname + '/index.html');
