937b71c2005fdeaefdd25b6de365a85b26554b7c
Michael Durfey <mdurfey@me.com>
2017-09-05 08:23:57 -0700

    collision map JSON edits

--- public/index.js
+++ public/index.js
@@ -1,88 +1,91 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, null, { preload: preload, create: create, update: update, render: render });
 
 function preload() {
 
-    game.load.tilemap('map', 'assets/tilemaps/maps/collision_test.json', null, Phaser.Tilemap.TILED_JSON);
-    game.load.image('ground_1x1', 'assets/tilemaps/tiles/ground_1x1.png');
-    game.load.image('walls_1x2', 'assets/tilemaps/tiles/walls_1x2.png');
-    game.load.image('tiles2', 'assets/tilemaps/tiles/tiles2.png');
-    game.load.image('cokecan', 'assets/sprites/cokecan.png');
+    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+
+
+    // game.load.tilemap('map', 'assets/tilemaps/maps/collision_test.json', null, Phaser.Tilemap.TILED_JSON);
+    // game.load.image('kenney', 'assets/tilemaps/tiles/kenney.png');
+    // game.load.image('super_mario', 'assets/tilemaps/tiles/super_mario.png');
+    // game.load.image('kenny_ground', 'assets/tilemaps/tiles/kenny_ground_64x64.png');
+    // game.load.image('cokecan', 'assets/sprites/cokecan.png');
 
 }
 
 var ship;
 var map;
 var layer;
-var cursors;
+var cursors;   
 
 function create() {
 
-    game.physics.startSystem(Phaser.Physics.P2JS);
+    // game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    game.stage.backgroundColor = '#2d2d2d';
+    // game.stage.backgroundColor = '#2d2d2d';
 
-    map = game.add.tilemap('map');
+    // map = game.add.tilemap('map');
 
-    map.addTilesetImage('ground_1x1');
-    map.addTilesetImage('walls_1x2');
-    map.addTilesetImage('tiles2');
+    // map.addTilesetImage('kenney');
+    // map.addTilesetImage('super_mario');
+    // map.addTilesetImage('kenny');
     
-    layer = map.createLayer('Tile Layer 1');
+    // layer = map.createLayer('Tile Layer 1');
 
-    layer.resizeWorld();
+    // layer.resizeWorld();
 
     //  Set the tiles for collision.
     //  Do this BEFORE generating the p2 bodies below.
-    map.setCollisionBetween(1, 12);
+    // map.setCollisionBetween(1, 12);
 
-    //  Convert the tilemap layer into bodies. Only tiles that collide (see above) are created.
-    //  This call returns an array of body objects which you can perform addition actions on if
-    //  required. There is also a parameter to control optimising the map build.
-    game.physics.p2.convertTilemap(map, layer);
+    // //  Convert the tilemap layer into bodies. Only tiles that collide (see above) are created.
+    // //  This call returns an array of body objects which you can perform addition actions on if
+    // //  required. There is also a parameter to control optimising the map build.
+    // game.physics.ARCADE.convertTilemap(map, layer);
 
-    ship = game.add.sprite(200, 200, 'cokecan');
-    game.physics.p2.enable(ship);
+    // ship = game.add.sprite(200, 200, 'cokecan');
+    // game.physics.enable(ship, Phaser.Physics.ARCADE);
 
-    game.camera.follow(ship);
+    // game.camera.follow(ship, FOLLOW_TOPDOWN_TIGHT);
 
-    //  By default the ship will collide with the World bounds,
-    //  however because you have changed the size of the world (via layer.resizeWorld) to match the tilemap
-    //  you need to rebuild the physics world boundary as well. The following
-    //  line does that. The first 4 parameters control if you need a boundary on the left, right, top and bottom of your world.
-    //  The final parameter (false) controls if the boundary should use its own collision group or not. In this case we don't require
-    //  that, so it's set to false. But if you had custom collision groups set-up then you would need this set to true.
-    game.physics.p2.setBoundsToWorld(true, true, true, true, false);
+    // //  By default the ship will collide with the World bounds,
+    // //  however because you have changed the size of the world (via layer.resizeWorld) to match the tilemap
+    // //  you need to rebuild the physics world boundary as well. The following
+    // //  line does that. The first 4 parameters control if you need a boundary on the left, right, top and bottom of your world.
+    // //  The final parameter (false) controls if the boundary should use its own collision group or not. In this case we don't require
+    // //  that, so it's set to false. But if you had custom collision groups set-up then you would need this set to true.
+    // game.physics.ARCADE.setBoundsToWorld(true, true, true, true, false);
 
-    //  Even after the world boundary is set-up you can still toggle if the ship collides or not with this:
-    // ship.body.collideWorldBounds = false;
+    // //  Even after the world boundary is set-up you can still toggle if the ship collides or not with this:
+    // // ship.body.collideWorldBounds = false;
 
-    cursors = game.input.keyboard.createCursorKeys();
+    // cursors = game.input.keyboard.createCursorKeys();
 
 }
 
 function update() {
 
-    if (cursors.left.isDown)
-    {
-        ship.body.rotateLeft(100);
-    }
-    else if (cursors.right.isDown)
-    {
-        ship.body.rotateRight(100);
-    }
-    else
-    {
-        ship.body.setZeroRotation();
-    }
+    // if (cursors.left.isDown)
+    // {
+    //     ship.body.rotateLeft(100);
+    // }
+    // else if (cursors.right.isDown)
+    // {
+    //     ship.body.rotateRight(100);
+    // }
+    // else
+    // {
+    //     ship.body.setZeroRotation();
+    // }
 
-    if (cursors.up.isDown)
-    {
-        ship.body.thrust(400);
-    }
-    else if (cursors.down.isDown)
-    {
-        ship.body.reverse(400);
-    }
+    // if (cursors.up.isDown)
+    // {
+    //     ship.body.thrust(400);
+    // }
+    // else if (cursors.down.isDown)
+    // {
+    //     ship.body.reverse(400);
+    // }
 
 }
 
